14229320777e6c30ef8ac91186bd669d86da8289
LuMi <lumigu.dev@gmail.com>
2022-07-07 15:44:26 -0500

    adding js funtionalities to the sections and btns

---
+++ app.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll(".section");
+const sectionBtns = document.querySelectorAll(".controlls");
+const sectionBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function transitions() {
+  //button click active
+  for (let i = 0; i < sectionBtn.length; i++) {
+    sectionBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
+
+  //sections active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from the other btns
+      sectionBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+}
+transitions();
